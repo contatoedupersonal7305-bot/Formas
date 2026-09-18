821951cdc080a6eb8acc6258124321f7bd23e2bc
EDUARDO MIRANDA DE CARVALHO <26210909@dsn127m1249386.cfp127aluno.local>
2026-09-18 15:56:20 -0300

    codigo de aprendizado completo

--- lista.py
+++ lista.py
@@ -1,2 +1,10 @@
 notas = [7.0, 1.5, 10.0, 8.5, 3.0]
-print(notas[1])
+
+# contador = 0
+# limite = len(notas) - 1
+
+# while contador <= 4:
+#         print(f"Sua nota é: {notas[contador]}")
+#         contador += 1
+for x in notas:
+        print(f"Sua nota é: {x}")
